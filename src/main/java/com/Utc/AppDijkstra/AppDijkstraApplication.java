e6f236454d532e4c3ad0cd8916cebaf837ee83b0
kevin <kevin@gmail.com>
2026-06-29 10:50:27 -0500

    creacion del proyecto

---
+++ src/main/java/com/Utc/AppDijkstra/AppDijkstraApplication.java
@@ -0,0 +1,13 @@
+package com.Utc.AppDijkstra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDijkstraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDijkstraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Utc/AppDijkstra/AppDijkstraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Utc.AppDijkstra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDijkstraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
