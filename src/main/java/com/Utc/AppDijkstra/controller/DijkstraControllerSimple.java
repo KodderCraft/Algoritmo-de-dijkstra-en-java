17b57ff0c08c3ccd32f989568be763f5fd6663cc
Kooder <codygaamez@gmail.com>
2026-06-30 18:53:59 -0500

    App web Funcional

--- src/main/java/com/Utc/AppDijkstra/controller/DijkstraControllerSimple.java
+++ src/main/java/com/Utc/AppDijkstra/controller/DijkstraControllerSimple.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api/dijkstra")
+@RequestMapping("/index")
 public class DijkstraControllerSimple {
 
     private Grafo grafo = new Grafo();
